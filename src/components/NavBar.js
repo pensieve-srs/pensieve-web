f04add78c1aadebdf4a3a98d91b6fd54acd5f1b9
nlaz <nikolazaris@utexas.edu>
2018-05-31 17:41:51 -0400

    Update navbar with dropdown and gravatar image

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,66 +3,80 @@ import { Link } from "react-router-dom";
 import cookie from "js-cookie";
 import cx from "classnames";
 import { withRouter } from "react-router";
+import { Image, Dropdown } from "semantic-ui-react";
+import Octicon from "react-octicon";
+import gravatar from "gravatar";
+
+import "./NavBar.css";
 
 class NavBar extends Component {
-  state = { isCollapsed: false };
-
-  onCollapse = () => this.setState(({ isCollapsed }) => ({ isCollapsed: !isCollapsed }));
-
   render() {
     const { className } = this.props;
-    const { isCollapsed } = this.state;
     const authenticated = Boolean(cookie.get("token"));
+    const user = cookie.get("user");
+
+    const email = user ? JSON.parse(user).email : "";
 
     const isLandingPage = this.props.location.pathname === "/";
 
     return (
       <nav
-        className={cx(className, "navbar navbar-expand-sm navbar-dark")}
-        style={{ zIndex: "2000", backgroundColor: isLandingPage ? "#1574fb" : "#262E45" }}
+        className={cx(className, "navbar navbar-expand-sm navbar-dark", {
+          "navbar-landing": isLandingPage,
+        })}
       >
         <div className="container">
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand" to={isLandingPage ? "/" : "/decks"}>
             Pensieve
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            aria-label="Toggle navigation"
-            onClick={this.onCollapse}
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
 
-          <div className={cx("navbar-collapse", { collapse: !isCollapsed })}>
-            {authenticated ? (
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link to="/decks" className="nav-link" onClick={this.onCollapse}>
-                    Decks
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/settings" className="nav-link" onClick={this.onCollapse}>
-                    Settings
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/logout" className="nav-link" onClick={this.onCollapse}>
-                    Logout
-                  </Link>
-                </li>
-              </ul>
-            ) : (
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link to="/login" className="nav-link" onClick={this.onCollapse}>
-                    Login
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </div>
+          {authenticated ? (
+            <ul className="navbar-nav ml-auto flex-row">
+              <li className="nav-item d-flex align-items-center mr-2">
+                <Link
+                  to="/decks/new"
+                  className="nav-link text-big text-light"
+                  style={{ fontSize: "20px" }}
+                >
+                  <Octicon name="plus" />
+                </Link>
+              </li>
+              <li className="nav-item d-flex align-items-center">
+                <Dropdown
+                  pointing="top right"
+                  direction="left"
+                  trigger={
+                    <Image
+                      style={{ height: "36px", width: "36px", margin: "0" }}
+                      src={gravatar.url(email)}
+                      avatar
+                    />
+                  }
+                >
+                  <Dropdown.Menu>
+                    <Dropdown.Item>
+                      <Link to="/settings" className="text-secondary text-uppercase">
+                        My Profile
+                      </Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <Link to="/logout" className="text-secondary text-uppercase">
+                        Logout
+                      </Link>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+            </ul>
+          ) : (
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/login" className="nav-link" onClick={this.onCollapse}>
+                  Login
+                </Link>
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
     );
