ff70a0c990d1ab9e4498cb454336fa54cf47df5c
nlaz <nikolazaris@utexas.edu>
2018-07-05 18:44:18 -0400

    Create page and form for resetting passwords

--- src/app/auth/authActions.js
+++ src/app/auth/authActions.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
 export const loginUser = (email, password) => {
-  return axios.post(`/api/users/login`, { email, password });
+  return axios.post("/api/users/login", { email, password });
 };
 
 export const signupUser = ({ email, password, name, invite }) => {
-  return axios.post(`/api/users/signup`, { email, password, name, invite });
+  return axios.post("/api/users/signup", { email, password, name, invite });
+};
+
+export const resetPassword = ({ token, newPassword, verifyPassword }) => {
+  const config = { headers: { Authorization: token } };
+
+  return axios.post("/api/users/reset_password", { newPassword, verifyPassword }, config);
 };
--- src/app/auth/index.js
+++ src/app/auth/index.js
@@ -2,5 +2,6 @@ import Login from "./Login";
 import Logout from "./Logout";
 import ReqAuth from "./ReqAuth";
 import Signup from "./Signup";
+import ResetPassword from "./ResetPassword";
 
-export { Login, Logout, ReqAuth, Signup };
+export { Login, Logout, ReqAuth, Signup, ResetPassword };
