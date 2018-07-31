e1154a30ef9df3a28b9648589d7a8ded80986684
Niko Lazaris <nikolazaris@gmail.com>
2018-07-31 15:56:50 -0400

    Update landing page (#76)
    * Update header
    
    * Update landing sections
    
    * Update landing sections

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Grid, Header, Image, Label, Responsive } from "semantic-ui-react";
+import { Container, Grid, Header, Image, Responsive } from "semantic-ui-react";
 
 import BetaListForm from "./BetaListForm";
 
@@ -12,116 +12,92 @@ class Landing extends Component {
     return (
       <div className="landing-page">
         <div className="landing-hero py-5 px-2">
-          <Container textAlign="center">
-            <div className="pb-5 mb-5 mt-3">
-              <Header inverted className="mb-4">
-                <Header.Content style={{ fontSize: "34px", lineHeight: "1em" }}>
-                  Improve your memory
+          <Container textAlign="left" className="d-flex flex-column flex-md-row">
+            <div className="hero-text my-auto col-md-5 mr-5 py-5">
+              <Header inverted className="mb-5">
+                <Header.Content style={{ fontSize: "32px", lineHeight: "1em" }}>
+                  Spaced Repetition Flashcards
                 </Header.Content>
-                <Header.Subheader
-                  className="my-3 mx-auto"
-                  as="h2"
-                  style={{ maxWidth: "550px", fontSize: "18px" }}
-                >
-                  Pensieve uses smarter flashcards with spaced repetition to help you conquer your
-                  memory
+                <Header.Subheader className="my-3" as="h2" style={{ fontSize: "22px" }}>
+                  A scheduling tool for smarter flashcards to keep your memories sharp
                 </Header.Subheader>
               </Header>
-              <div className="text-center d-table mr-auto ml-auto">
-                <BetaListForm inverted />
-              </div>
+              <BetaListForm inverted />
             </div>
-            <Responsive minWidth={768}>
-              <div style={{ marginBottom: "-300px" }}>
-                <Image centered src={require("./web_mobile.png")} />
-              </div>
+            <Responsive minWidth={768} className="col-md-7" style={{ minWidth: "1000px" }}>
+              <Image centered src={require("./web_mobile.png")} />
             </Responsive>
             <Responsive className="text-center" maxWidth={767}>
-              <Image style={{ marginBottom: "-270px" }} centered src={require("./mobile.png")} />
+              <Image
+                centered
+                style={{ marginBottom: "-270px", maxWidth: "300px" }}
+                src={require("./mobile.png")}
+              />
             </Responsive>
           </Container>
         </div>
-        <div className="landing-howItWorks border bg-white border-bottom py-5">
-          <div className="container py-5 my-4">
-            <div className="row">
-              <div className="col-sm-10 offset-sm-1 col-md-8 offset-md-2">
-                <Header as="h2" textAlign="center" size="large" className="mb-4">
+        <div className="landing-howItWorks bg-white">
+          <div className="container">
+            <div className="row d-flex flex-column flex-md-row align-content-center">
+              <div className="info-text col-md-5 py-4">
+                <Header as="h2" size="large" className="text-uppercase mb-2 text-blue">
                   Use it or lose it
                 </Header>
-                <p className="lead text-dark text-center">
-                  When you are learning, 80% of what you hear, read, and write will be forgotten
-                  tomorrow. Practice and repetition will help you remember anything you want to
-                  learn. Practice too soon though and you have wasted your time. Practice too late
-                  and you've forgotten the material and have to relearn it. {" "}
-                  <strong>
-                    With Pensieve, you review at the perfect time so you spend less effort to learn
-                    what you want to remember.
-                  </strong>
+                <p className="lead text-dark">
+                  80% of what you hear, read, and write will be forgotten tomorrow. Repetition will
+                  help you remember anything you want to learn.{" "}
+                  <span className="font-weight-medium">
+                    Pensieve allows you to practice at the perfect time so you spend less effort to
+                    learn what you want.
+                  </span>
                 </p>
+              </div>
+              <div className="col-md-7 my-auto">
+                <Image centered src={require("./graph.png")} />
               </div>
             </div>
           </div>
         </div>
-        <div className="landing-box py-5">
-          <div className="container my-5 py-3">
-            <Header as="h2" textAlign="center" size="large" className="mb-4">
-              How it works
-            </Header>
+        <div className="landing-box">
+          <div className="container">
             <Grid columns={3} padded relaxed stackable>
               <Grid.Row>
                 <Grid.Column>
-                  <Header as="h3" className="mb-2">
-                    <Label color="grey" size="large" className="ml-0 mr-2">
-                      1
-                    </Label>Create your cards
-                  </Header>
-                  <p className="text-secondary">
-                    Collect the information you want to learn and input them in our system as
-                    flashcards. We keep these organized for you and will predict the card strength
-                    in your memory over time.
-                  </p>
+                  <div className="border-box p-3 mx-3">
+                    <Header as="h3" className="text-uppercase">
+                      Supplement your learning
+                    </Header>
+                  </div>
                 </Grid.Column>
                 <Grid.Column>
-                  <Header as="h3" className="mb-2">
-                    <Label color="grey" size="large" className="ml-0 mr-2">
-                      2
-                    </Label>Get notified
-                  </Header>
-                  <p className="text-secondary">
-                    Your recall ability of these cards will decrease constantly as you start to
-                    forget them. We will estimate the best time to review them and let you know when
-                    you should study them again.
-                  </p>
+                  <div className="border-box p-3 mx-3">
+                    <Header as="h3" className="text-uppercase">
+                      Boost your professional expertise
+                    </Header>
+                  </div>
                 </Grid.Column>
                 <Grid.Column>
-                  <Header as="h3" className="mb-2">
-                    <Label color="grey" size="large" className="ml-0 mr-2">
-                      3
-                    </Label>Review your cards
-                  </Header>
-                  <p className="text-secondary m-0">
-                    Once your cards expired, you will need to study them again, telling us how well
-                    you remember them. The harder a card is to remember, the sooner you will see it
-                    again.
-                  </p>
+                  <div className="border-box p-3 mx-3">
+                    <Header as="h3" className="text-uppercase">
+                      Keep your memories sharp
+                    </Header>
+                  </div>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
           </div>
         </div>
-        <div className="landing-info border py-5">
-          <div className="container py-5">
+        <div className="landing-info">
+          <div className="container">
             <div className="row">
               <div className="col-sm-10 offset-sm-1 col-md-8 offset-md-2">
-                <Header as="h2" textAlign="center" size="large" className="mb-4">
-                  Pensieve is for people who want to learn better
+                <Header as="h2" textAlign="center" size="large" className="mb-2">
+                  For people who want to learn better
                 </Header>
                 <p className="lead text-dark mx-2 text-center">
-                  Pensieve is for students, language learners, and lifelong learners that are tired
-                  of forgetting information after just learning it. It is for people that don't have
-                  time to review their notes everyday. It can be used for anything you want to
-                  remember: <strong>languages</strong>, <strong>medical terms</strong>,{" "}
-                  <strong>books</strong>, or even <strong>friend's names</strong>.
+                  Pensieve is for developers, language learners, and book readers that are tired of
+                  forgetting information after just learning it. Pensieve gives you control of your
+                  memory so you hold on to critical information longer.
                 </p>
                 <h4 className="text-center text-dark mt-5 pt-3 mb-3">
                   Improve your memory for free
